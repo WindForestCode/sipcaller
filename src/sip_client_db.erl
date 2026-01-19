788b8491a54ce1cd8559b3f7068dba9463078179
WindForest <munkored@yandex.ru>
2026-01-19 09:18:35 +0700

    refactored - creating db in module app now

--- src/sip_client_db.erl
+++ src/sip_client_db.erl
@@ -1,29 +1,9 @@
 
 -module(sip_client_db).
 
--behaviour(gen_server).
-
 -include("include/uri.hrl").
 
--export([start_link/0, save_contact/1, get_contact/1, create_tables/0]).
--export([init/1, handle_call/3, handle_cast/2, terminate/2]).
-
-start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
-
-%% Callback functions
-init([]) ->
-    create_tables(),
-    {ok, null}.
-
-handle_call(_Request, _From, LoopData) ->
-    {reply, null, LoopData}.
-
-handle_cast(_Msg, LoopData) ->
-    {noreply, LoopData}.
-
-terminate(_Reason, _LoopData) ->
-    close_tables().
+-export([save_contact/1, get_contact/1]).
 
 %% API
 save_contact(Contact) ->
@@ -38,13 +18,3 @@ get_contact(UserId) ->
         [{BinaryId, Contact}] -> Contact;
         [] -> user_not_found
     end.
-
-create_tables() ->
-    ets:new(usersRam, [named_table,
-        set,
-        {keypos, 1},
-        public]),
-    io:format("ETS storage created~n").
-
-close_tables() ->
-    ets:delete(usersRam).
--- src/sip_pingpong_app.erl
+++ src/sip_pingpong_app.erl
@@ -10,9 +10,21 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    create_tables(),
     Res = sip_pingpong_sup:start_link(),
     sip_pingpong_sup:dump(),
     Res.
 
 stop(_State) ->
+    close_tables(),
     ok.
+
+create_tables() ->
+    ets:new(usersRam, [named_table,
+        set,
+        {keypos, 1},
+        public]),
+    io:format("ETS storage created~n").
+
+close_tables() ->
+    ets:delete(usersRam).
