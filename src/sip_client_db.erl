92f94cd425c501bd93bb3f8182534f96fde9ddf7
WindForest <munkored@yandex.ru>
2026-01-21 10:11:19 +0700

    added -spec

--- src/sip_client_db.erl
+++ src/sip_client_db.erl
@@ -6,12 +6,14 @@
 -export([save_contact/1, get_contact/1]).
 
 %% API
+-spec save_contact([tuple()]) -> true.
 save_contact(Contact) ->
     io:format("sip database - trying to save Contact - ~p~n", [Contact]),
     [Tuple | _ ] = Contact, 
     UserId = element(3, Tuple),
     ets:insert(usersRam, {UserId, Contact}).
 
+-spec get_contact(integer()) -> tuple() | user_not_found.
 get_contact(UserId) ->
     BinaryId = integer_to_binary(UserId),
     case ets:lookup(usersRam, BinaryId) of
